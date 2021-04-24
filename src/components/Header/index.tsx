c3f4e667a65fa9b9d45303b6299e081f265e5058
gguilherme42 <exemplo@teste.com>
2021-04-23 23:05:27 -0300

    Criação do componente Header.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
+export const Header: React.FC = () => {
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR,
+    })
+
+    return (
+        <header className={styles.headerContainer}>
+            <img src="/logo.svg" alt="Podecastr"/>
+
+            <p>O melhor para você ouvir, sempre</p>
+
+            <span>{currentDate}</span>
+        </header>
+    );
+}
