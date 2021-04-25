720de58387163f7c9db3d859ce05e20cc8e59151
gguilherme42 <58306122+gguilherme42@users.noreply.github.com>
2021-04-25 17:34:09 -0300

    - Importação e uso de Image, format, parseISO, ptBR, convertDurationToTimeString e styles; - Adição de mais campoas na interface IEpisode; - Mudança na interface IHomeProps, ao invés de episodes, agora há latestEpisodes e allEpisodes; - Formatação dos dados das props antes do retorno da API; - Criação do html para a parte de últimos episódios.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,72 +1,79 @@
 import {GetStaticProps} from 'next';
+import Image from 'next/image';
+
+import {format, parseISO} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
-/* 
-Requisição no modelo SPA
-  useEffect(() => {
-      fetch('http://localhost:3333/episodes')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
- */
+import styles from './home.module.scss';
 
-/*
-Requisição no medo SSR
-  exportando um função com esse nome o NextJS irá entender que a função deve ser executada antes do componente. 
 
-  export async function getServerSideProps() {
-    const response = await fetch('http://localhost:3333/episodes')
-    const data = await response.json()
-
-    return {
-      props: {
-        episodes: data,
-      }
-    }
-
-  }
-
-*/
-
-/*
-Requisição no modelo SSG
-  exportando um função com esse nome o NextJS irá entender que a função deve ser executada antes do componente. 
-  export async function getStaticProps() {
-    const response = await fetch('http://localhost:3333/episodes')
-    const data = await response.json()
-
-    return {
-      props: {
-        episodes: data,
-      },
-      revalidate: 60 * 60 * 8, // 8 horas
-    }
-  }
-
-*/
 
 interface IEpisode {
   id: string;
   title: string;
   members: string;
+  publishedAt: string;
+  thumbnail: string;
+  description: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
 }
 
 interface IHomeProps {
-  episodes: IEpisode[]
+  latestEpisodes: IEpisode[],
+  allEpisodes: IEpisode[];
 }
 
-export default function Home(props: IHomeProps) {
+export default function Home({ latestEpisodes
+, allEpisodes }: IHomeProps) {
 
 
   return (
-    <h1>Index</h1>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+        
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <Image 
+                  width={192} 
+                  height={192} 
+                  src={episode.thumbnail} 
+                  alt={episode.title} 
+                  objectFit="cover"  
+                />
+
+                <div className={styles.episodeDetails}> 
+                  <a href="">{episode.title}</a>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episódio"/>
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+      </section>  
+    </div>
   )
 }
 
 
 export const getStaticProps: GetStaticProps =  async () => {
-  const { data } = await api.get('/episodes',
+  const { data } = await api.get('episodes',
   {
     params: {
       _limit: 12,
@@ -75,9 +82,30 @@ export const getStaticProps: GetStaticProps =  async () => {
     }
   })
 
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR}),
+      thumbnail: episode.thumbnail,
+      description: episode.description,
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      url: episode.file.url,
+
+    }
+  })
+
+
+  const latestEpisodes =  episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length)
+
   return {
     props: {
-      episodes: data,
+       latestEpisodes,
+       allEpisodes,
     },
     revalidate: 60 * 60 * 8, // 8 horas
   }
